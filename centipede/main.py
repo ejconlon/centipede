a315409203533aaa29c69df4ec8c08c33e676243
Eric Conlon <37287+ejconlon@users.noreply.github.com>
2025-06-28 13:53:30 -0700

    etc

--- centipede/main.py
+++ centipede/main.py
@@ -1,5 +1,41 @@
+import math
+
+import numpy as np
+import numpy.typing as npt
+import plotext as plt
+
+type Time = float
+type Freq = float
+type Phase = float
+type Rate = int
+type Space = npt.NDArray[npt.int32]
+type Array = npt.NDArray[npt.float32]
+
+
+TAU = 2 * math.pi
+
+
 def main():
     print("Hello, world!")
+
+
+def mk_space(start: Time, end: Time, rate: Rate) -> Space:
+    assert start <= end
+    assert rate > 0
+    start_point = round(start * rate)
+    end_point = round(end * rate)
+    num = end_point - start_point
+    return np.linspace(start_point, end_point, num=num)
+
+
+def mk_sin(spc: Space, freq: Freq, phase: Phase) -> Array:
+    assert freq > 0
+    return np.sin(spc * TAU + phase)
+
+
+def plot(spc: Space, arr: Array) -> None:
+    plt.plot(spc, arr)
+    plt.show()
 
 
 if __name__ == "__main__":
