d20f1a04a04882befb9916e79c707657ca96ed4b
Eric Conlon <37287+ejconlon@users.noreply.github.com>
2025-06-28 17:47:25 -0700

    etc

--- centipede/main.py
+++ centipede/main.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 import numpy.typing as npt
 import plotext as plt
@@ -8,11 +6,11 @@ type Time = float
 type Freq = float
 type Phase = float
 type Rate = int
-type Space = npt.NDArray[npt.int32]
-type Array = npt.NDArray[npt.float32]
+type Space = npt.NDArray[np.int64]
+type Array = npt.NDArray[np.float64]
 
 
-TAU = 2 * math.pi
+TAU = np.float64(2 * np.pi)
 
 
 def main():
@@ -23,14 +21,14 @@ def mk_space(start: Time, end: Time, rate: Rate) -> Space:
     assert start <= end
     assert rate > 0
     start_point = round(start * rate)
-    end_point = round(end * rate)
-    num = end_point - start_point
-    return np.linspace(start_point, end_point, num=num)
+    end_point = max(start_point, round(end * rate) - 1)
+    num = end_point - start_point + 1
+    return np.linspace(start=start_point, stop=end_point, num=num, dtype=np.int64)
 
 
-def mk_sin(spc: Space, freq: Freq, phase: Phase) -> Array:
+def mk_sin(spc: Space, rate: Rate, freq: Freq, phase: Phase) -> Array:
     assert freq > 0
-    return np.sin(spc * TAU + phase)
+    return np.sin(np.mod(spc.astype(np.float64) * (freq * TAU / rate) + phase, TAU))
 
 
 def plot(spc: Space, arr: Array) -> None:
